74aa2db4ba078debde51b412028a6084e895d17e
Zabi <zbvmihai@gmail.com>
2023-03-05 16:13:13 +0000

    add addition, subtract, multiplication and division functionality

--- app/src/main/java/com/zabava/mycalculator/MainActivity.kt
+++ app/src/main/java/com/zabava/mycalculator/MainActivity.kt
@@ -10,8 +10,8 @@ import org.w3c.dom.Text
 class MainActivity : AppCompatActivity() {
 
     private var tvInput: TextView? = null
-    var lastNumeric : Boolean = false
-    var lastDot : Boolean = false
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,16 +40,23 @@ class MainActivity : AppCompatActivity() {
             btnZero, btnOne, btnTwo, btnThree, btnFour,
             btnFive, btnSix, btnSeven, btnEight, btnNine
         )
+
+        val operators = listOf(btnAdd, btnDivide, btnMultiply, btnSubtract)
+
+        for (i in operators) {
+            i.setOnClickListener { onOperator(i) }
+        }
+
         for (i in numbers) {
             i.setOnClickListener {
                 onDigit(i)
                 lastNumeric = true
             }
-
         }
 
         btnClear.setOnClickListener { onClear(btnClear) }
         btnDot.setOnClickListener { onDecimalPoint(btnDot) }
+        btnEqual.setOnClickListener { onEqual(btnEqual) }
 
     }
 
@@ -65,11 +72,89 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onDecimalPoint(view: View) {
-        if (lastNumeric && !lastDot){
+        if (lastNumeric && !lastDot) {
             tvInput?.append(".")
             lastNumeric = false
             lastDot = true
         }
     }
 
+    private fun onOperator(view: View) {
+        tvInput?.text?.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
+        }
+    }
+
+    private fun onEqual(view: View) {
+        if (lastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+
+            try {
+
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+
+                if (tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+
+                } else if (tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = (one.toDouble() + two.toDouble()).toString()
+
+                } else if (tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = (one.toDouble() / two.toDouble()).toString()
+
+                } else if (tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+                    tvInput?.text = (one.toDouble() * two.toDouble()).toString()
+                }
+            } catch (e: java.lang.ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
 }
